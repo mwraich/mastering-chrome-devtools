d7e8e7fd7d0f1ea32367c25776f75e00786b37d8
Meep <mwraich28@gmail.com>
2021-03-25 10:30:00 -0400

    wrap up debugging, page jank, srcset exercises

--- public/js/mark-and-measure.js
+++ public/js/mark-and-measure.js
@@ -1,9 +1,13 @@
-window.addEventListener("load", thingToMeasure);
+window.addEventListener('load', thingToMeasure);
 
 function thingToMeasure() {
-  fetch("/api")
-    .then(result => result.json())
-    .then(json => {
-      json.images.forEach(image => console.log(image.name));
-    });
+	performance.mark('start');
+	fetch('/api')
+		.then((result) => result.json())
+		.then((json) => {
+			json.images.forEach((image) => console.log(image.name));
+		});
+	performance.mark('end');
+	performance.measure('Our Measurement', 'start', 'end');
+	performance.getEntriesByType('measure');
 }
--- public/js/page-jank.js
+++ public/js/page-jank.js
@@ -1,7 +1,7 @@
-const appRoot = document.getElementById("root");
-const addButton = document.querySelector(".add");
-const removeButton = document.querySelector(".remove");
-const originalLogo = document.querySelector(".jank-logo");
+const appRoot = document.getElementById('root');
+const addButton = document.querySelector('.add');
+const removeButton = document.querySelector('.remove');
+const originalLogo = document.querySelector('.jank-logo');
 
 let rAF;
 let allLogos;
@@ -12,73 +12,72 @@ let logoSize = originalLogo.getBoundingClientRect();
 let maxHeight = Math.floor(bodySize.height - logoSize.height);
 
 function init() {
-  if (allLogos) {
-    bodySize = document.body.getBoundingClientRect();
-    for (let i = 0; i < allLogos.length; i++) {
-      document.body.removeChild(allLogos[i]);
-    }
-    document.body.appendChild(originalLogo);
-    logoSize = originalLogo.getBoundingClientRect();
-    document.body.removeChild(originalLogo);
-    maxHeight = Math.floor(bodySize.height - logoSize.height);
-  }
-  for (let i = 0; i < count; i++) {
-    let newLogo = originalLogo.cloneNode();
-    let top = Math.floor(Math.random() * maxHeight);
-    if (top === maxHeight) {
-      newLogo.classList.add("up");
-    } else {
-      newLogo.classList.add("down");
-    }
-    newLogo.style.left = i / (count / 97) + "vw";
-    newLogo.style.top = top + "px";
-    document.body.appendChild(newLogo);
-  }
+	if (allLogos) {
+		bodySize = document.body.getBoundingClientRect();
+		for (let i = 0; i < allLogos.length; i++) {
+			document.body.removeChild(allLogos[i]);
+		}
+		document.body.appendChild(originalLogo);
+		logoSize = originalLogo.getBoundingClientRect();
+		document.body.removeChild(originalLogo);
+		maxHeight = Math.floor(bodySize.height - logoSize.height);
+	}
+	for (let i = 0; i < count; i++) {
+		let newLogo = originalLogo.cloneNode();
+		let top = Math.floor(Math.random() * maxHeight);
+		if (top === maxHeight) {
+			newLogo.classList.add('up');
+		} else {
+			newLogo.classList.add('down');
+		}
+		newLogo.style.left = i / (count / 97) + 'vw';
+		newLogo.style.top = top + 'px';
+		document.body.appendChild(newLogo);
+	}
 
-  allLogos = document.querySelectorAll(".jank-logo");
-  count = allLogos.length;
+	allLogos = document.querySelectorAll('.jank-logo');
+	count = allLogos.length;
 }
 
 function move() {
-  for (let i = 0; i < allLogos.length; i++) {
-    let currentLogo = allLogos[i];
-    let currentLogoPosition = currentLogo.classList.contains("down")
-      ? currentLogo.offsetTop + distance
-      : currentLogo.offsetTop - distance;
-    if (currentLogoPosition < 0) currentLogoPosition = 0;
-    if (currentLogoPosition > maxHeight) currentLogoPosition = maxHeight;
-    currentLogo.style.top = currentLogoPosition + "px";
-    if (currentLogo.offsetTop === 0) {
-      currentLogo.classList.remove("up");
-      currentLogo.classList.add("down");
-    }
-    if (currentLogo.offsetTop === maxHeight) {
-      currentLogo.classList.remove("down");
-      currentLogo.classList.add("up");
-    }
-  }
-
-  rAF = window.requestAnimationFrame(move);
+	// for (let i = 0; i < allLogos.length; i++) {
+	//   let currentLogo = allLogos[i];
+	//   let currentLogoPosition = currentLogo.classList.contains("down")
+	//     ? currentLogo.offsetTop + distance
+	//     : currentLogo.offsetTop - distance;
+	//   if (currentLogoPosition < 0) currentLogoPosition = 0;
+	//   if (currentLogoPosition > maxHeight) currentLogoPosition = maxHeight;
+	//   currentLogo.style.top = currentLogoPosition + "px";
+	//   if (currentLogo.offsetTop === 0) {
+	//     currentLogo.classList.remove("up");
+	//     currentLogo.classList.add("down");
+	//   }
+	//   if (currentLogo.offsetTop === maxHeight) {
+	//     currentLogo.classList.remove("down");
+	//     currentLogo.classList.add("up");
+	//   }
+	// }
+	// rAF = window.requestAnimationFrame(move);
 }
 
-addButton.addEventListener("click", () => {
-  count = count + 50;
-  init();
+addButton.addEventListener('click', () => {
+	count = count + 50;
+	init();
 });
 
-removeButton.addEventListener("click", () => {
-  if (count >= 50) {
-    count = count - 50;
-  }
-  init();
+removeButton.addEventListener('click', () => {
+	if (count >= 50) {
+		count = count - 50;
+	}
+	init();
 });
 
-window.addEventListener("resize", () => {
-  init();
+window.addEventListener('resize', () => {
+	init();
 });
 
-window.addEventListener("load", () => {
-  appRoot.removeChild(originalLogo);
-  init();
-  rAF = window.requestAnimationFrame(move);
+window.addEventListener('load', () => {
+	appRoot.removeChild(originalLogo);
+	init();
+	rAF = window.requestAnimationFrame(move);
 });
